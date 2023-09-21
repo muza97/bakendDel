23759e86129a4839783a1d7a8720e28454c2bd6c
muza97 <mustafa.zaitoun97@gmail.com>
2023-09-21 15:51:23 +0200

    mindre ändringar

--- functions/server.js
+++ functions/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({status: "ok"});
+  res.json({ status: "ok" });
+});
+
+const API_URL = 'https://thronesapi.com/api/v2/Characters'; // Använd din API-länk här
+
+app.get('/fetch-data', async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 module.exports = app;
--- functions/service.js
+++ functions/service.js
@@ -1,8 +1,4 @@
-require('dotenv').config(),
+const functions = require('firebase-functions');
 const app = require('./server');
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+exports.api = functions.https.onRequest(app);
