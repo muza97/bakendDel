8e87f5adc73203bf1bee90577d88a9a3f82a1581
muza97 <mustafa.zaitoun97@gmail.com>
2023-10-02 14:46:46 +0200

    skapat data samt json fil

--- functions/server.js
+++ functions/server.js
@@ -1,34 +1,52 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const fs = require ('fs');
-const cors = require ('cors');
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
 
-
-// Middleware för att tolka JSON-förfrågningar
-app.use (express.json);
+// Middleware
 app.use(bodyParser.json());
-app.use (cors());
 
-// Array för att lagra data
+// Läs in befintlig data från JSON-fil
+const dataFilePath = path.join(__dirname, 'bilar.json');
 let bilar = [];
 
-fs.readFile('bilar.json', 'utf8',(err,data)=> {
-  if(!err){
-    bilar = JSON.parse(data);
-  }
-})
+try {
+    const rawData = fs.readFileSync(dataFilePath, 'utf8');
+    bilar = JSON.parse(rawData);
+} catch (err) {
+    console.error("Fel vid inläsning av bilar.json:", err);
+}
 
-// POST-rutt för att lägga till data i "bilar"
-app.post('/bilar', (req, res) => {
-  const nyBil = req.body;
-  data.push(nyBil);
-  res.json({ meddelande: 'Bilen har lagts till i "bilar".' });
-});
-
-// GET-rutt för att hämta data från "data"
+// GET-anrop
 app.get('/bilar', (req, res) => {
-  res.json(bilar);
+    res.json(bilar);
 });
 
+// POST-anrop
+app.post('/bilar', (req, res) => {
+    let nyaBilar = req.body;
+
+    if (!Array.isArray(nyaBilar)) {
+        nyaBilar = [nyaBilar];  // Gör en enskild bil till en lista med en bil
+    }
+
+    for (const bil of nyaBilar) {
+        const { id, name, modell, farg } = bil;
+
+        // Kontrollera om alla fält finns
+        if (!id || !name || !modell || !farg) {
+            return res.status(400).json({ error: `Alla fält (id, name, modell, farg) är obligatoriska. Problem med bilen: ${JSON.stringify(bil)}` });
+        }
+
+        bilar.push(bil);
+    }
+
+    // Spara till JSON-fil
+    fs.writeFileSync(dataFilePath, JSON.stringify(bilar, null, 2), 'utf8');
+
+    res.json({ meddelande: `${nyaBilar.length} bil(ar) har lagts till.`, bilar: nyaBilar });
+});
+
+module.exports = app;
