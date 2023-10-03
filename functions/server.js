9b27ba05fec8978e59aa84bade24b5ccefc2f254
muza97 <mustafa.zaitoun97@gmail.com>
2023-10-03 13:44:56 +0200

    lite ändringar

--- functions/server.js
+++ functions/server.js
@@ -49,4 +49,52 @@ app.post('/bilar', (req, res) => {
     res.json({ meddelande: `${nyaBilar.length} bil(ar) har lagts till.`, bilar: nyaBilar });
 });
 
+app.delete('/bilar/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10); // Ta ID från URL-parametern
+
+  // Hitta indexet för bilen med det angivna ID:t
+  const index = bilar.findIndex(bil => bil.id === id);
+
+  if (index === -1) {
+      return res.status(404).json({ error: 'Bil med angivet ID hittades inte.' });
+  }
+
+  // Ta bort bilen från listan
+  bilar.splice(index, 1);
+
+  // Spara den uppdaterade listan till JSON-fil
+  fs.writeFileSync(dataFilePath, JSON.stringify(bilar, null, 2), 'utf8');
+
+  res.json({ meddelande: `Bil med ID ${id} har tagits bort.` });
+});
+
+app.patch('/bilar/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10); // Ta ID från URL-parametern
+
+  // Hitta bilen med det angivna ID:t
+  const bil = bilar.find(b => b.id === id);
+
+  if (!bil) {
+      return res.status(404).json({ error: 'Bil med angivet ID hittades inte.' });
+  }
+
+  // Uppdatera bilens egenskaper
+  if (req.body.name) {
+      bil.name = req.body.name;
+  }
+  if (req.body.modell) {
+      bil.modell = req.body.modell;
+  }
+  if (req.body.farg) {
+      bil.farg = req.body.farg;
+  }
+
+  // Spara den uppdaterade listan till JSON-fil
+  fs.writeFileSync(dataFilePath, JSON.stringify(bilar, null, 2), 'utf8');
+
+  res.json({ meddelande: `Bil med ID ${id} har uppdaterats.`, uppdateradBil: bil });
+});
+
+
+
 module.exports = app;
